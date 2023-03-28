f7ef0fd2d5cebd715da0b48dc945f3395556056e
PlapaKermit <92321646+PlapaKermit@users.noreply.github.com>
2023-03-28 09:25:00 +0200

    Exo 2

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 import java.awt.*;
 
 public class Main {
-    public static void main(String[] args) {
+    //Exo 1
+    /*public static void main(String[] args) {
         System.out.println("Hello world!");
         Voiture v1 = new Voiture();
         Voiture v2 = new Voiture(4, "Peugeot", Color.red);
@@ -9,6 +10,15 @@ public class Main {
         System.out.println(v2.toString());
         v1.nbportes = 5;
         System.out.println(v1.toString());
+    }*/
+    //Exo 2
+    public static void main(String[] args) {
+        Student s1 = new Student(1, "John", "Smith");
+        Student s2 = new Student(2, "Mary", "Sue");
+        System.out.println(s1.toString());
+        System.out.println(s2.toString());
+        s1.setFirstName("Johnathan");
+        System.out.println(s1.toString());
     }
 
 }
---
+++ Student.java
@@ -0,0 +1,40 @@
+public class Student {
+    public int id;
+    public String firstName;
+    public String lastName;
+
+    public Student(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return id + ":" + firstName + " " + lastName;
+    }
+}
